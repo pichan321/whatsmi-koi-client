22f9ae01f24cee2884a6dcda236b3d3990d144c8
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-10-29 02:23:19 -0400

    Added more styling to pages

---
+++ src/components/feed/feed.js
@@ -0,0 +1,27 @@
+import { API_URL } from "../../../variables";
+import Image from "next/image";
+
+export default function Feed({ data }) {
+  return (
+    <div className="p-12">
+      <div className="grid grid-cols-12 gap-7">
+        {data &&
+          data.map((each) => {
+            const src = `${API_URL}\\${each}`;
+            return (
+              <div className="col-span-12 md:col-span-4 lg:col-span-3 xl:col-span-2 rounded-2xl h-full max-h-48">
+                <Image
+                  loader={() => src}
+                  src={src}
+                  width={500}
+                  height={500}
+                  className="w-full h-full object-fill rounded-md shadow-lg"
+                  style={{ objectFit: "fill" }}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import NavBar from '@/components/navbar/navbar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { API_URL } from '../../variables';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import Feed from '@/components/feed/feed';
 // const inter = Inter({ subsets: ['latin'] })
+import { motion } from "framer-motion"
 
 export default function Home() {
   const [image, setImage] = useState(null);
-  
+  const [feed, setFeed] = useState([])
   const uploadImage = async () => {
     try {
 
@@ -17,28 +20,60 @@ export default function Home() {
       })
 
       console.log(response)
+
+      fetchFeed()
     } catch {
       
     } 
     // var response = await axios.get();
   }
 
+  const fetchFeed = async () => {
+    try {
+      var response = await axios.get(API_URL + "/feed")
+      setFeed(response.data)
+    } catch {
+
+    }
+  }
+
+
+  useEffect(() => {
+    fetchFeed()
+  }, [])
+
   return (
     // < className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
     // </main>  
-    <div className='p-5'>
+    <div className='p-5 background'>
         <NavBar/>
-
+      
         <div className='p-10'>
           <input type="file" id="img" name="img" accept="image/*" onChange={(e) => setImage(e.target.files[0])}/>
-
-          {
-            image && <Image src={URL.createObjectURL(image)} alt="my-koi" width={300} height={300} className='rounded-lg'/>
-          }
-
-          <button className='bg-blue-500 rounded-lg p-5' onClick={() => uploadImage()}>Identify</button>
+          <Button colorScheme='teal' className='bg-blue-500 rounded-lg p-5' onClick={() => uploadImage()}>Classify</Button>
         </div>
+
+        <div className='flex flex-row'>
+          <div>
+          {
+            image && <motion.div
+                initial={{opacity: "0%"}}
+                animate={{opacity: "100%"}}
+                transition={{ ease: "easeInOut", duration: 1 }}
+        
+            >
+              <Image src={URL.createObjectURL(image)} alt="my-koi" width={300} height={300} className='rounded-2xl shadow-lg'/>
+            </motion.div>
+          }
+          </div>
+ 
+        </div>
+
+
+          <Feed data={feed}/>
         
     </div>
+
+
   )
 }
